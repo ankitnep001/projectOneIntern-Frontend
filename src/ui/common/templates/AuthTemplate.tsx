b2d1ab6d869c41cbbc03210209c4654aca4d0cda
Ankit <ankit001nep@gmail.com>
2024-06-20 12:37:32 +0545

    feat:localization in authForms Login.tsx & SignUp.tsx

--- src/ui/common/templates/AuthTemplate.tsx
+++ src/ui/common/templates/AuthTemplate.tsx
@@ -1,12 +1,15 @@
+import { projectLabel } from '@config/constant/project'
+import useLang from '@hooks/useLang'
 import { Outlet } from 'react-router-dom'
 import Logo from '../molecules/Logo'
 
 const AuthTemplate = () => {
+  const { lang } = useLang()
   return (
     <div className='flex flex-col justify-center items-center h-screen bg-slate-100'>
       <div className='flex flex-col items-center mb-5'>
         <Logo />
-        <p className='font-bold text-sm md:text-md'>Mid-baneshwor, Kathmandu</p>
+        <p className='font-bold text-sm md:text-md'>{projectLabel.address[lang]}</p>
       </div>
       <div className=' bg-white rounded-md shadow-md'>
         <Outlet />
--- src/ui/user/pages/auth/SignUp.tsx
+++ src/ui/user/pages/auth/SignUp.tsx
@@ -1,3 +1,5 @@
+import { authLabel } from '@data/localization/common/auth'
+import useLang from '@hooks/useLang'
 import Button from '@ui/common/atoms/Button'
 import InputField from '@ui/common/atoms/InputField'
 import Label from '@ui/common/atoms/Label'
@@ -7,40 +9,40 @@ import { IoArrowBack, IoMailUnreadOutline } from 'react-icons/io5'
 import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+  const { lang } = useLang();
   const navigate = useNavigate();
   const goBack = () => {
-    navigate(-1); // Navigate back one step in history
+    navigate('/'); // Navigate back one step in history
   };
 
   return (
     <div className='px-4 py-4  w-[98vw] md:w-[70vw] lg:w-[41vw]'>
       <form className=''>
-        <div className='w-fit flex justify-end items-center px-2 mb-2 cursor-pointer hover:bg-[#1a496d] bg-[#255d88] rounded-md text-white ' onClick={goBack}>
-          <IoArrowBack />
-          <Button type={'button'} buttonText={'Go Back'} />
+        <div className='w-fit pb-2'>
+          <Button type={'button'} buttonText={authLabel.goBack[lang]} icon={<IoArrowBack />} onClick={goBack} />
         </div>
-        <h1 className='text-[#255d88] font-bold mb-3'>User Login</h1>
+        <h1 className='text-[#255d88] font-bold mb-3'>{authLabel.userSignup[lang]}</h1>
 
         <div className='w-full'>
           {/* Name */}
           <div className=' flex flex-col md:flex-row gap-x-4'>
             {/* First Name */}
             <div className='relative'>
-              <Label name={'firstName'} label={'First Name'} required={true} />
+              <Label name={'firstName'} label={authLabel.firstName[lang]} required={true} />
               <FiUser className='absolute left-3 top-[39px] ' />
               <InputField name={'firstName'} type={'text'} placeholder={'Enter your First Name'} />
             </div>
 
             {/* Middle Name */}
             <div className='relative'>
-              <Label name={'middleName'} label={'Middle Name'} />
+              <Label name={'middleName'} label={authLabel.middleName[lang]} />
               <FiUser className='absolute left-3 top-[39px] ' />
               <InputField name={'middleName'} type={'text'} placeholder={'Enter your Middle Name'} />
             </div>
 
             {/* Last Name */}
             <div className='relative'>
-              <Label name={'lastName'} label={'Last Name'} required={true} />
+              <Label name={'lastName'} label={authLabel.lastName[lang]} required={true} />
               <FiUser className='absolute left-3 top-[39px] ' />
               <InputField name={'lastName'} type={'text'} placeholder={'Enter your Last Name'} />
             </div>
@@ -49,18 +51,18 @@ const SignUp = () => {
           {/* Email and Mobile number */}
           <div className='flex flex-col md:flex-row gap-x-4 '>
             <div className='relative w-full'>
-              <Label name={'email'} label={'Email'} required={true} />
+              <Label name={'email'} label={authLabel.email[lang]} required={true} />
               <IoMailUnreadOutline className='absolute left-3 top-[38px] ' />
               <InputField name={'email'} type={'email'} autocomplete='on' placeholder={'Enter your Email'} />
             </div>
 
             <div className='relative w-full'>
-              <Label name={'phoneNumber'} label={'Mobile Number'} required={true} />
+              <Label name={'phoneNumber'} label={authLabel.mobileNumber[lang]} required={true} />
               <FiPhone className='absolute left-3 top-[39px]' />
               <InputField
                 name={'phoneNumber'}
                 type={'tel'}
-                placeholder={'Enter your Mobile Number'}
+                placeholder={authLabel.enterYourMobileNumber[lang]}
               />
             </div>
           </div>
@@ -68,32 +70,32 @@ const SignUp = () => {
           {/* Password */}
           <div className='flex flex-col md:flex-row gap-x-4 '>
             <div className='relative w-full'>
-              <Label name={'password'} label={'Password'} required={true} />
+              <Label name={'password'} label={authLabel.password[lang]} required={true} />
               <AiOutlineLock className='absolute left-3 top-[38px]' />
-              <InputField name={'password'} type={'password'} placeholder={'Password'} />
+              <InputField name={'password'} type={'password'} placeholder={authLabel.enterNewPassword[lang]} />
             </div>
 
             <div className='relative w-full'>
-              <Label name={'confirmPassword'} label={'Confirm Password'} required={true} />
+              <Label name={'confirmPassword'} label={authLabel.confirmPassword[lang]} required={true} />
               <AiOutlineLock className='absolute left-3 top-[38px]' />
               <InputField
                 name={'confirmPassword'}
                 type={'password'}
-                placeholder={'Confirm Password'}
+                placeholder={authLabel.confirmYourPassword[lang]}
               />
             </div>
           </div>
 
           {/* Button */}
           <div className='hover:bg-[#1a496d] bg-[#255d88] rounded-md cursor-pointer text-white my-2  w-full self-center'>
-            <Button type={'submit'} buttonText={'Sign Up'} />
+            <Button type={'submit'} buttonText={authLabel.signup[lang]} />
           </div>
         </div>
       </form>
       <div className='flex'>
-        <p className=''>Already have an account? </p>&nbsp;
+        <p className=''>{authLabel.alreadyHaveAnAccount[lang]} </p>&nbsp;
         <Link to='/auth/user'>
-          <span className='text-[#255d88] font-bold'>Login </span>
+          <span className='text-[#255d88] font-bold'>{authLabel.login[lang]}</span>
         </Link>
       </div>
 
